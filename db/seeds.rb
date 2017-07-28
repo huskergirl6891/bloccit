f213e1b6e8ef1e1a40893127c111eebf076893d3
Carissa Castro <carissabcastro@gmail.com>
2017-07-28 10:11:34 -0500

    Assignment 17 Seeding Data complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,36 @@
 require 'random_data'
 
-# Create posts
-50.times do
-  Post.create!(
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
-  )
-end
+# # Create posts
+# 1.times do
+#   Post.create!(
+#     title: RandomData.random_sentence,
+#     body: RandomData.random_paragraph
+#   )
+# end
+#
+# posts = Post.all
+#
+# # Create comments
+# 1.times do
+#   Comment.create!(
+#     post: posts.sample,
+#     body: RandomData.random_paragraph
+#   )
+# end
 
-posts = Post.all
+# Create unique post
+unique_post = Post.find_or_create_by!(
+  title: "Unique Title",
+  body: "Unique Body"
+)
 
-# Create comments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
-end
+
+# Create unique comment related to unique post
+Comment.find_or_create_by!(
+  post: unique_post,
+  body: "Unique Comment Body"
+)
+
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
