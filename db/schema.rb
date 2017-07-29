22a313d748318318f8bbd0a2e392d826e04c4fe1
Carissa Castro <carissabcastro@gmail.com>
2017-07-28 19:56:38 -0500

    Fixed answers migration

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 20170728013114) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  add_index "answers", ["post_id"], name: "index_answers_on_post_id"
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
