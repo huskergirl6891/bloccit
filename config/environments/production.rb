24c7c8f27ed6bd3d0e1ed3465308d873ce6960f6
Carissa Castro <carissabcastro@gmail.com>
2017-08-08 21:51:24 -0500

    Checkpoint 31 Favorites complete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'murmuring-brook-85658.herokuapp.com' }
 end
